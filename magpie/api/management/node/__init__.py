582b94322c756db3da715bf3f36d38eff2bbbba3
alexandercyu <alexanderc.yu@utoronto.ca>
2023-03-09 16:22:10 -0500

    Update __init__.py
    Moved register node routes from register/__init__.py to node/__init__.py

--- magpie/api/management/node/__init__.py
+++ magpie/api/management/node/__init__.py
@@ -7,3 +7,12 @@ def includeme(config):
     LOGGER.info("Adding API node...")
     config.add_route(**s.service_api_route_info(s.NodeAdminAPI))
 
+    # Add route for receiving info about a node
+    config.add_route(**s.service_api_route_info(s.RegisterNodeAPI))
+
+    # Add route for receiving info about a node through a URL
+    config.add_route(**s.service_api_route_info(s.RegisterNodeURLAPI))
+
+    # Add route for receiving info about a node through a file such as JSON
+    config.add_route(**s.service_api_route_info(s.RegisterNodeJSONAPI))
+
